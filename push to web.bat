579d699cd882473c51e5af3d6ea63bd57a63982a
Kumbirai-Bodzo <bodzok@gmail.com>
2020-01-18 12:40:29 +0200

    first commit

---
+++ push to web.bat
@@ -0,0 +1,4 @@
+
+git remote remove origin
+git remote add origin https://github.com/Kumbirai-Bodzo/mine.git
+git push -u origin master
---
+++ run git command.bat
@@ -0,0 +1,12 @@
+@echo on
+cls
+:start
+type nul > README.md
+git init
+
+git add -A
+
+git commit -m "commit"
+git status
+
+
